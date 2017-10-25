a14108f614bef71874dc7e28aaae9af99377f17e
Corey Clark <clarkcj@cox.net>
2017-10-24 21:11:44 -0400

    Updated File Structure

--- src/main/java/me.robertod26.firework/Firework.java
+++ src/main/java/me.robertod26.firework/Firework.java
@@ -16,22 +16,30 @@ import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener {
+public class Firework extends JavaPlugin implements Listener {
     public final Logger logger = Logger.getLogger("Minecraft");
-    public static Main plugin;
-    
-    public void onDisable(){
-        PluginDescriptionFile pdfFile = this.getDescription();
-        this.logger.info(pdfFile.getName() + " Has Been Disabled!");
-    }
-    
+    public static Firework instance;
+
+    @Override
     public void onEnable(){
-        PluginDescriptionFile pdfFile = this.getDescription();
-        this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has Been Enabeled!");
-        getServer().getPluginManager().registerEvents(this. this);
-        System.out.println("Welcome To FireworkJoin By RobertoD26 and Clarkcj");
+        instance = this;
+        enableMessage();
+        new FireworkConfig();
     }
-    
+
+    private void enableMessage(){
+        getLogger().info("|======================================|");
+        getLogger().info("| Plugin: FireworkJoin                 |");
+        getLogger().info("| Created By: Clarkcj                  |");
+        getLogger().info("| Contributions By: RobertoD26         |");
+        getLogger().info("| Version: 0.5-dev                     |");
+        getLogger().info("|======================================|");
+    }
+
+    public static Firework getInstance(){
+        return instance;
+    }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e){
         player p = e.getPlayer();
